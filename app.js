738af06d60bb2bf5f1e4d3a45c90d27412c98066
anantsinghchauhan <57144246+anantsinghchauhan@users.noreply.github.com>
2021-03-28 17:18:46 +0530

    uploaded

---
+++ app.js
@@ -0,0 +1,62 @@
+new hoverEffect({
+  parent: document.querySelector(".one .prjone"),
+  intensity: 0.1,
+  image1: "images/p1.webp",
+  image2: "images/p2.webp",
+  displacementImage: "heightMap.png",
+});
+
+new hoverEffect({
+  parent: document.querySelector(".tw .prjone "),
+  intensity: 0.1,
+  image1:  "images/p3.webp",
+  image2:  "images/p5.webp",
+  displacementImage: "heightMap.png",
+  hover: true
+});
+
+
+
+
+new LocomotiveScroll({
+  el: document.querySelector("#back"),
+  smooth: true,
+  multiplier: 1.5,
+  smoothMobile: true,
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,65 @@
+document.querySelector('#menu-open').addEventListener('click', ()=>{
+    document.querySelector('#slidingNavbar').style.left = '0';
+});
+
+
+document.querySelector('#closeBtn').addEventListener('click', function(){
+    document.querySelector('#slidingNavbar').style.left = '-100%';
+});
+
+
+
+let tl = gsap.timeline();
+
+tl.from("nav h1", {
+  opacity: 0,
+  duration: 1,
+  ease: "Power1.easeInOut",
+  x: 30,
+})
+.from('.mtxt .m',{
+    opacity: 0,
+    ease: "Expo.easeIn",
+    y: -2
+},'-=.1')
+.from('.mtxt .e',{
+    opacity: 0,
+    ease: "Expo.easeIn",
+    y: 2
+},'-=.5')
+.from('.mtxt .n',{
+    opacity: 0,
+    ease: "Expo.easeIn",
+    y: -2
+},'-=.4')
+.from('.mtxt .u',{
+    opacity: 0,
+    ease: "Expo.easeIn",
+    y: 2
+},'-=.5')
+.from('.main h1',{
+    opacity:0,
+    duration: 1,
+    ease: "Power1.easeInOut",
+    y: 30,
+},'-=1.5')
+.from('.hero-img',{
+    width: 0,
+    duration: 1,
+    ease: "Power1.easeInOut ",
+},'-=1.5')
+.from('.links a', 2 ,{
+    ease: 'Expo.easeInOut',
+    x: 50,
+    opacity: 0
+})
+
+
+
+let t2 = gsap.timeline();
+
+t2.from('.links a', 2 ,{
+    ease: 'Expo.easeInOut',
+    x: 50,
+    opacity: 0
+})
